c1854b8c24187585a7532559aa47ca7fab7e033b
16bitash <entropy.ash@gmail.com>
2022-08-18 21:28:30 +0530

    Add loader in the useEffect post

--- src/App12/index.js
+++ src/App12/index.js
@@ -4,25 +4,26 @@ import axios from "axios";
 
 const App12 = () => {
   const [posts, setPosts] = useState([]);
-  const [currentPostID, setCurrentPostID] = useState(null);
 
+  const [currentPostID, setCurrentPostID] = useState(null);
   const [specificPost, setSpecificPost] = useState({});
+
+  const [isLoading, setIsLoading] = useState(false);
+  console.log('isLoading', isLoading);
+  
 
   const fetchPosts = async () => {
     const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
     setPosts(res.data);
   };
 
   const fetchPost = async () => {
-    console.log("fetching post");
-
-    if (currentPostID === null) {
-      return;
-    }
-
+    setIsLoading(true);
     const res = await axios.get(
       `https://jsonplaceholder.typicode.com/posts/${currentPostID}`
     );
+    setIsLoading(false);
 
     setSpecificPost(res.data);
   };
@@ -32,6 +33,11 @@ const App12 = () => {
   }, []);
 
   useEffect(() => {
+    if (currentPostID === null) {
+      setSpecificPost({});
+      return;
+    }
+
     fetchPost();
   }, [currentPostID]);
 
@@ -39,6 +45,7 @@ const App12 = () => {
     return (
       <>
         <button onClick={() => setCurrentPostID(null)}>Back</button>
+        {isLoading && <div>Loading...</div>}
         <h1>{specificPost.title}</h1>
         <p>{specificPost.body}</p>
       </>
